895508831d26c3be13256b96dc84bcb5ffab571f
Neto Martins <netomartins2@hotmail.com>
2022-11-23 01:03:14 -0300

    add logo do projeto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import igniteLogo from "../assets/igniteLogo.svg";
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={igniteLogo} />
+      <img src={igniteLogo} alt="Logotipo" />
     </header>
   )
 }
