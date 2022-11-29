e78c7ef46a1d71f1654e5f6a7fe6d5bfc8fb3427
Neto Martins <netomartins2@hotmail.com>
2022-11-28 22:28:45 -0300

    ajustando component Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,32 @@ import { Post } from './Post';
 
 import './styles.css'
 
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar';
+
 function App() {
 
   return (
     <div>
 
       <Header />
-      <Post
-        autor="Neto Martins"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quibusdam dolor excepturi ut amet natus incidunt rerum at, id hic, qui quae ad eaque consequatur maiores deserunt, quasi itaque delectus!"
-      />
+      <div className={styles.wrapper}>
 
-      <Post
-        autor="Ivy Maria"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quibusdam dolor excepturi ut amet natus incidunt rerum at, id hic, qui quae ad eaque consequatur maiores deserunt, quasi itaque delectus!"
-      />
+        <Sidebar />
 
+        <main>
+          <Post
+            autor="Neto Martins"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quibusdam dolor excepturi ut amet natus incidunt rerum at, id hic, qui quae ad eaque consequatur maiores deserunt, quasi itaque delectus!"
+          />
 
-      <Post
-        autor="Vania"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quibusdam dolor excepturi ut amet natus incidunt rerum at, id hic, qui quae ad eaque consequatur maiores deserunt, quasi itaque delectus!"
-      />
+          <Post
+            autor="Ivy Maria"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quibusdam dolor excepturi ut amet natus incidunt rerum at, id hic, qui quae ad eaque consequatur maiores deserunt, quasi itaque delectus!"
+          />
 
-
+        </main>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,21 @@
-import { styles } from './Header.module.css'
+import styles from './Sidebar.module.css'
 
 export function Sidebar() {
   return (
-    <strong className={styles.header}>Ignite Feed</strong>
+    <aside className={styles.sidebar}>
+      <img src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=480&q=60" />
+      <div className={styles.profile}>
+        <strong>Neto Martins</strong>
+        <span>Web Developer</span>
+      </div>
+
+
+      <footer>
+        <a href='#'>
+          Editar o seu perfil
+        </a>
+      </footer>
+    </aside>
   );
 }
 
